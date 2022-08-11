57359da7736355c2792f7d06eb7d878d56754111
RomanPrygunkov <103605350+RomanPrygunkov@users.noreply.github.com>
2022-08-11 22:26:13 +0300

    fixed

--- js.js
+++ js.js
@@ -1,17 +1,22 @@
 const coffees = ["Latte", "Cappuccino", "Americano"];
-let coffeeIndex = 0
 let coffeeName = prompt("Поиск кофе по названию:", "").toLowerCase()
 coffeeName = coffeeName.charAt(0).toUpperCase() + coffeeName.slice(1)
 
-if (coffees.includes(coffeeName)) {
-    coffeeIndex = coffees.findIndex(item => {      // Посмотрел гайд на ютубе
-        if (item  === coffeeName) {
-            return true
-        }
-    } )
-    alert(`Держите ваш любимый кофе coffee. Он ${coffeeIndex + 1} по популярности в нашей кофейне.`)
+let coffeeIndex = coffees.findIndex(function (word) {
+    if (coffeeName === word) {
+        return true
+    }
+    })   
     
-} else {
-alert ("К сожалению, такого вида кофе нет в наличии")
-}
+coffeeIndex !== -1 ?
+    alert(`Держите ваш любимый кофе ${coffeeName}. Он ${coffeeIndex + 1} по популярности в нашей кофейне.`) :
+    alert("К сожалению, такого вида кофе нет в наличии")    
+
+
+  
+
+ 
+  
+
+ 
   
